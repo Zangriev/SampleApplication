60c51c09068773d74471fd874392e14c7065f6b5
Bastien Denet <bastien@alfa-safety.fr>
2017-05-04 15:41:14 +0200

    update

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 
 var express = require('express');
+var http = require('http');
 var app = express();
+var server = http.createServer(app);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(8080, function () {
+server.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
